07f71a9301604c4abab47ec94a4ffad8d02d6519
Don-001 <nirmalodedara007@gmail.com>
2025-01-27 07:56:30 +0530

    commit

--- connections.js
+++ connections.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose')
 
 async function connectMongoDb(url){
     await mongoose.connect(url)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB connection error:', err));
 }
 
 module.exports = {
