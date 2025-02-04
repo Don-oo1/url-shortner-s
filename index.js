e1d32369a67d33d7ab131e185aa51004a228a41c
Don-001 <nirmalodedara007@gmail.com>
2025-02-04 08:09:46 +0530

    generate shortId

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const shortId = require("short-id")
+const mongoose = require("mongoose")
+
+const app = express()
+const PORT = 8000
+
+//db connections
+const db = mongoose.connect("mongodb://127.0.0.1:27017/url-1").then(()=>{console.log("Db connected 🥳")})
+
+//db schema
+const urlSchema = {
+    redirectUrl:{
+        type:String,
+        required:true
+    },
+    shortId:{
+        type:String,
+        required:true,
+        unique:true,
+    }
+}
+
+//db model
+
+const Url = mongoose.model("urls", urlSchema)
+
+
+//modelwares
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+//routes
+app.post("/url",async (req, res)=>{
+    const redirectUrl = req.body.url
+    const shortIdUrl = shortId.generate()
+    await Url.create({
+        redirectUrl,
+        shortId:shortIdUrl,
+    })
+    res.json({shortId:shortIdUrl})
+})
+
+app.listen(PORT, ()=>{console.log("server started 🚀")})
